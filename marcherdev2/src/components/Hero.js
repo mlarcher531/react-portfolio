99ea4c8c99868e94d39b0533bfbf105cd0e6683c
mlarcher531 <mlarcher531@gmail.com>
2021-05-01 10:40:00 -0500

    initial commit

---
+++ marcherdev2/src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const hero = props => {
+
+    let content = (
+        <header>
+            <div class="name-box">
+                <h1>MIKE ARCHER</h1>
+            </div>
+            <p>DEVELOPER || PROBLEM-SOLVER || CREATIVE</p>
+        </header>
+    )
+
+    return content
+}
+
+export default hero
---
+++ marcherdev2/src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const nav = (props) => {
+
+    let content =(
+        <div class="navbar">
+        <nav>
+            <ul>
+                <li><a href="#about">ABOUT</a></li>
+                <li><a href="#work">WORK</a></li>
+                <li><a href="#contact">CONTACT</a></li>
+            </ul>
+        </nav>
+    </div>
+    )
+
+    return content
+}
+
+export default nav
